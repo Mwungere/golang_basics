2fdbce02782d3d7de520e68e1db6316268b4f74c
Mwungere <mwungereelite56@gmail.com>
2024-10-17 16:49:10 +0200

    booleans & conditionals

--- main.go
+++ main.go
@@ -1,38 +1,37 @@
+
 package main
 
 import (
 	"fmt"
-	
 )
 
+func main() {
+	age := 45
 
-func main(){
+	fmt.Println(age <= 50)
+	fmt.Println(age >= 50)
+	fmt.Println(age == 45)
+	fmt.Println(age != 50)
 
-	//loops
-
-	// similar to while loop
-	x := 0
-
-	for x < 5 {
-		fmt.Println("The value of x is: ", x)
-		x++
+	if age < 30 {
+		fmt.Println("age is less than 30")
+	} else if age < 40 {
+		fmt.Println("age is less than 40")
+	} else {
+		fmt.Println("age is not less than 40")
 	}
 
-	// for loop
+	names := []string{"mario", "luigi", "yoshi", "peach", "bowser"}
 
-	for y := 0; y<5; y++ {
-		fmt.Println("The value of y is: ", y)
-	}
-
-	names := []string{"John", "Paul", "George", "Ringo"}
-
-	for index, value := range names {
-		fmt.Printf("The value at index %v is %v\n", index, value)
-	}
-
-	//if you do not need to use the index or value just replace any with an underscore
-
-	for _, value := range names {
-		fmt.Printf("The value is %v\n", value)
+	for index, val := range names {
+		if index == 1 {
+			fmt.Println("continuing at pos", index)
+			continue
+		}
+		if index > 2 {
+			fmt.Println("breaking at pos", index)
+			break
+		}
+		fmt.Printf("the value at pos %v is %v \n", index, val)
 	}
 }
